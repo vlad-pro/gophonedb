d92c90d930056e4b4b8175384f895942b8e231f5
vproskurin <vladimir.proskurin@gmail.com>
2020-01-27 07:46:30 +0700

    refactored code

--- db/phone.go
+++ db/phone.go
@@ -1,9 +1,122 @@
 package db
 
-import "database/sql"
+import (
+	"database/sql"
+	// _ "github.com/lib/pq"
+)
+
+// Phone represents phone number
+type Phone struct {
+	ID     int
+	Number string
+}
+
+func Open(driverName, dataSource string) (*DB, error) {
+	db, err := sql.Open(driverName, dataSource)
+	if err != nil {
+		return nil, err
+	}
+	return &DB{db}, nil
+}
+
+type DB struct {
+	db *sql.DB
+}
+
+func (db *DB) Close() error {
+	return db.db.Close() // why double db?
+}
+
+func (db *DB) Seed() error {
+	data := []string{"1234567890",
+		"123 456 7891",
+		"(123) 456 7892",
+		"(123) 456-7893",
+		"123-456-7894",
+		"123-456-7890",
+		"1234567892",
+		"(123)456-7892",
+	}
+	for _, number := range data {
+		if _, err := insertPhone(db.db, number); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func insertPhone(db *sql.DB, phone string) (int, error) {
+	statement := `INSERT INTO PHONE_NUMBERS(value) VALUES($1) RETURNING id`
+	var id int
+	err := db.QueryRow(statement, phone).Scan(&id)
+	if err != nil {
+		return -1, err
+	}
+	return id, nil
+}
+
+func (db *DB) AllPhones() ([]Phone, error) {
+	rows, err := db.db.Query("SELECT id, value FROM phone_numbers")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var ret []Phone
+
+	for rows.Next() {
+		var p Phone
+		if err := rows.Scan(&p.ID, &p.Number); err != nil {
+			return nil, err
+		}
+		ret = append(ret, p)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return ret, nil
+}
+
+// var ErrNoRecord = errors.new("") to make package db agnostic and to hide
+// the use of sql package.
+
+func (db *DB) FindPhone(number string) (*Phone, error) {
+	var p Phone
+	err := db.db.QueryRow("SELECT * FROM phone_numbers WHERE value=$1", number).Scan(&p.ID, &p.Number)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil // ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+	return &p, nil
+}
+
+func (db *DB) UpdatePhone(p *Phone) error {
+	statement := `UPDATE phone_numbers SET value=$2 WHERE id=$1`
+	_, err := db.db.Exec(statement, p.ID, p.Number)
+	return err
+}
+
+func (db *DB) DeletePhone(id int) error {
+	statement := `DELETE FROM phone_numbers WHERE id=$1`
+	_, err := db.db.Exec(statement, id)
+	return err
+}
 
 func Migrate(driverName, dataSource string) error {
-
+	db, err := sql.Open(driverName, dataSource)
+	if err != nil {
+		return err
+	}
+	err = createPhoneNumbersTable(db)
+	if err != nil {
+		return err
+	}
+	return db.Close()
 }
 
 func createPhoneNumbersTable(db *sql.DB) error {
@@ -43,4 +156,14 @@ func createDB(db *sql.DB, name string) error {
 		panic(err)
 	}
 	return nil
+}
+
+// We do not use this right now
+func getPhone(db *sql.DB, id int) (string, error) {
+	var number string
+	err := db.QueryRow("SELECT * FROM phone_numbers WHERE id=$1", id).Scan(&id, &number)
+	if err != nil {
+		return "", err
+	}
+	return number, nil
 }
